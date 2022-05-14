75867ee347e2900511090ba12c6b1aab0e726dae
mojaeya <mojaeya@gmail.com>
2022-05-14 17:51:43 +0900

    0516 문제 정답

--- 2. Answer/0516 - 기본문제.js
+++ 2. Answer/0516 - 기본문제.js
@@ -1,1 +1,92 @@
+// 🐯 1. A를 #으로
+function solution(s) {
+  let answer = "";
+  for (let x of s) {
+    if (x == "A") answer += "#";
+    else answer += x;
+  }
+  return answer;
+}
 
+let str = "BANANA";
+console.log(solution(str));
+
+// 🐯 2. 문자 찾기
+function solution(s, t) {
+  let answer = 0;
+  for (let x of s) {
+    if (x === t) answer++;
+  }
+  return answer;
+}
+
+let str = "COMPUTERPROGRAMMING";
+console.log(solution(str, "R"));
+
+// 🐯 3. 대문자
+function solution(s) {
+  let answer = 0;
+  for (let x of s) {
+    //let num=x.charCodeAt();
+    //if(num>=65 && num<=90) answer++;
+    if (x === x.toUpperCase()) answer++;
+  }
+
+  return answer;
+}
+
+let str = "JavaScriptAlgorithmHorangStudy";
+console.log(solution(str));
+
+// 🐯 4. 가장 긴 문자열
+function solution(s) {
+  let answer = "",
+    max = Number.MIN_SAFE_INTEGER;
+  for (let x of s) {
+    if (x.length > max) {
+      max = x.length;
+      answer = x;
+    }
+  }
+  return answer;
+}
+let str = ["teacher", "time", "student", "beautiful", "good"];
+console.log(solution(str));
+
+// 🐯 5. 가운데 문자 출력
+function solution(s) {
+  let answer;
+  let mid = Math.floor(s.length / 2);
+  if (s.length % 2 === 1) answer = s.substring(mid, mid + 1);
+  else answer = s.substring(mid - 1, mid + 1);
+  //if(s.length%2===1) answer=s.substr(mid, 1);
+  //else answer=s.substr(mid-1, 2);
+  return answer;
+}
+console.log(solution("study"));
+
+// 🐯 6. 중복문자제거
+function solution(s) {
+  let answer = "";
+  //console.log(s.indexOf("K"));
+  for (let i = 0; i < s.length; i++) {
+    //console.log(s[i], i, s.indexOf(s[i]));
+    if (s.indexOf(s[i]) === i) answer += s[i];
+  }
+  return answer;
+}
+console.log(solution("ksekkset"));
+
+// 🐯 7. 중복단어제거
+function solution(s) {
+  let answer;
+  //console.log(s.indexOf("student"));
+  answer = s.filter((v, i) => {
+    //console.log(v, i);
+    if (s.indexOf(v) === i) return v;
+  });
+
+  return answer;
+}
+let str = ["good", "time", "good", "time", "student"];
+console.log(solution(str));
