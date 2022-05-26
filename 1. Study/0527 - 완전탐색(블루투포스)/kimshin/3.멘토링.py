2f18f09014671d0cb046cf836aa3d23ad6deb352
kyle-shk <guqtls85@naver.com>
2022-05-27 01:27:26 +0900

    brute force

---
+++ 1. Study/0527 - 완전탐색(블루투포스)/kimshin/3.멘토링.py
@@ -0,0 +1,25 @@
+def sol(test):
+    answer=0
+    m=len(test)
+    n=len(test[0])
+
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            cnt=0
+            for k in range(m):
+                pi=pj=0
+                for s in range(n):
+                    if test[k][s] == i:
+                        pi=s
+                    if test[k][s] == j:
+                        pj = s
+                if pi < pj:
+                    cnt+=1
+            if cnt ==m:
+                answer+=1
+    return answer
+
+
+
+arr=[[3,4,1,2],[4,3,2,1],[3,1,4,2]]
+print(sol(arr))
