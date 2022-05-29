c6b49fc1d46c39d3886f8ede03f73649b66cd64f
mojaeya <mojaeya@gmail.com>
2022-05-29 21:50:27 +0900

    0530 mojaeya 2

--- 1. Study/0530 - 효율성(투포인터 알고리즘)/mojaeya/4_연속 부분수열 2.js
+++ 1. Study/0530 - 효율성(투포인터 알고리즘)/mojaeya/4_연속 부분수열 2.js
@@ -1,23 +1,17 @@
-// let arr = [1, 3, 1, 2, 3];
+let arr = [1, 3, 1, 2, 3];
 
-// console.log(solution(5, arr));
+console.log(solution(5, arr));
 
-// function solution(M, arr) {
-//   let answer = 0;
-//   let sum = 0;
-//   let end = 0;
-//   for (let st = 0; st < arr.length; st++) {
-//     while (end < arr.length) {
-//       sum += arr[end];
-//       if (sum <= M) {
-//         answer++;
-//       } else {
-//         end = st++;
-//         sum = 0;
-//         break;
-//       }
-//       end++;
-//     }
-//   }
-//   return answer;
-// }
+function solution(M, arr) {
+  let answer = 0;
+  let sum = 0;
+  let start = 0;
+  for (let end = 0; end < arr.length; end++) {
+    while (sum <= M) {
+      sum += arr[end];
+      answer++;
+    }
+    if (sum > M) sum -= arr[start++];
+  }
+  return answer;
+}
