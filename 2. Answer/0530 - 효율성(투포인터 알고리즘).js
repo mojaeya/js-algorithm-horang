30814bb0f5e3db4067eb3a3fc5c2c0bce36545e8
mojaeya <mojaeya@gmail.com>
2022-05-29 16:08:39 +0900

    0530 문제 정답

--- 2. Answer/0530 - 효율성(투포인터 알고리즘).js
+++ 2. Answer/0530 - 효율성(투포인터 알고리즘).js
@@ -1,13 +1,73 @@
 // 🐯 1.
+function solution(arr1, arr2){
+    let answer=[];
+    let n=arr1.length;
+    let m=arr2.length;
+    let p1=p2=0;
+    while(p1<n && p2<m){
+        if(arr1[p1]<=arr2[p2]) answer.push(arr1[p1++]);
+        else answer.push(arr2[p2++]);
+    }
+    while(p1<n) answer.push(arr1[p1++]);
+    while(p2<m) answer.push(arr2[p2++]); 
+    return answer;
+}
+
+let a=[1, 3, 5];
+let b=[2, 3, 6, 7, 9];
+console.log(solution(a, b));
 
 // 🐯 2.
+function solution(arr1, arr2){
+    let answer=[];
+    arr1.sort();
+    arr2.sort();
+    let p1=p2=0;
+    while(p1<arr1.length && p2<arr2.length){
+        if(arr1[p1]==arr2[p2]){
+            answer.push(arr1[p1++]);
+            p2++;
+        }
+        else if(arr1[p1]<arr2[p2]) p1++;
+        else p2++;
+    }              
+    return answer;
+}
+
+let a=[1, 3, 9, 5, 2];
+let b=[3, 2, 5, 7, 8];
+console.log(solution(a, b));
 
 // 🐯 3.
+function solution(m, arr){
+    let answer=0, lt=0, sum=0;
+    for(let rt=0; rt<arr.length; rt++){
+        sum+=arr[rt];
+        if(sum===m) answer++;
+        while(sum>=m){
+            sum-=arr[lt++];
+            if(sum===m) answer++;       
+        }
+    }        
+    return answer;
+}
+
+let a=[1, 2, 1, 3, 1, 1, 1, 2];
+console.log(solution(6, a));
+</script>
 
 // 🐯 4.
+function solution(m, arr){
+    let answer=0, sum=0, lt=0;
+    for(let rt=0; rt<arr.length; rt++){
+        sum+=arr[rt];
+        while(sum>m){
+            sum-=arr[lt++];
+        }
+        answer+=(rt-lt+1);
+    }               
+    return answer;
+}
 
-// 🐯 5.
-
-// 🐯 6.
-
-// 🐯 7.
+let a=[1, 3, 1, 2, 3];
+console.log(solution(5, a));
