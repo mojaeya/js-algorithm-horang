d5b3387547d91fe35f9e7250ddf705a5e88a2e45
mojaeya <mojaeya@gmail.com>
2022-06-05 23:32:59 +0900

    0606 문제 정답

--- 2. Answer/0606 - 자료구조(스택).js
+++ 2. Answer/0606 - 자료구조(스택).js
@@ -1,13 +1,65 @@
-// 🐯 1.
+// 🐯 1. 올바른 괄호
+function solution(s) {
+  let answer = "YES";
+  stack = [];
+  for (let x of s) {
+    if (x === "(") stack.push(x);
+    else {
+      if (stack.length === 0) return "NO";
+      stack.pop();
+    }
+  }
+  if (stack.length > 0) return "NO";
+  return answer;
+}
 
-// 🐯 2.
+let a = "(()(()))(()";
+console.log(solution(a));
 
-// 🐯 3.
+// 🐯 2. 괄호문자제거
+function solution(s) {
+  let answer;
+  let stack = [];
+  for (let x of s) {
+    if (x === ")") {
+      while (stack.pop() !== "(");
+    } else stack.push(x);
+  }
+  answer = stack.join("");
+  return answer;
+}
 
-// 🐯 4.
+let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+console.log(solution(str));
 
-// 🐯 5.
+// 🐯 3. 크레인 인형 뽑기
+function solution(board, moves) {
+  let answer = 0;
+  let stack = [];
+  moves.forEach((pos) => {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][pos - 1] !== 0) {
+        let tmp = board[i][pos - 1];
+        board[i][pos - 1] = 0;
+        if (tmp === stack[stack.length - 1]) {
+          stack.pop();
+          answer += 2;
+        } else stack.push(tmp);
+        break;
+      }
+    }
+  });
 
-// 🐯 6.
+  return answer;
+}
 
-// 🐯 7.
+let a = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(a, b));
