26006489d062326f4ae87e442fa2cfd958a2ec18
mojaeya <mojaeya@gmail.com>
2022-06-09 21:00:17 +0900

    0610 문제 정답

--- 2. Answer/0610 - 자료구조(스택,큐).js
+++ 2. Answer/0610 - 자료구조(스택,큐).js
@@ -1,13 +1,71 @@
-// 🐯 1.
+// 🐯 1. 후위식 연산
+function solution(s) {
+  let answer;
+  let stack = [];
+  for (let x of s) {
+    if (!isNaN(x)) stack.push(Number(x));
+    else {
+      let rt = stack.pop();
+      let lt = stack.pop();
+      if (x === "+") stack.push(lt + rt);
+      else if (x === "-") stack.push(lt - rt);
+      else if (x === "*") stack.push(lt * rt);
+      else if (x === "/") stack.push(lt / rt);
+    }
+  }
+  answer = stack[0];
+  return answer;
+}
 
-// 🐯 2.
+let str = "352+*9-";
+console.log(solution(str));
 
-// 🐯 3.
+// 🐯 2. 쇠막대기
+function solution(s) {
+  let answer = 0;
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") stack.push("(");
+    else {
+      stack.pop();
+      if (s[i - 1] === "(") answer += stack.length;
+      else answer++;
+      //stack.pop(); 이 위치에 하면 레이저까지 카운팅한다.
+    }
+  }
+  return answer;
+}
 
-// 🐯 4.
+let a = "()(((()())(())()))(())";
+console.log(solution(a));
 
-// 🐯 5.
+// 🐯 3. 공주구하기
+function solution(n, k) {
+  let answer;
+  let queue = Array.from({ length: n }, (v, i) => i + 1);
+  while (queue.length) {
+    for (let i = 1; i < k; i++) queue.push(queue.shift());
+    queue.shift();
+    if (queue.length === 1) answer = queue.shift();
+  }
+  return answer;
+}
 
-// 🐯 6.
+console.log(solution(8, 3));
 
-// 🐯 7.
+// 🐯 4. 교육과정설계
+function solution(need, plan) {
+  let answer = "YES";
+  let queue = need.split("");
+  for (let x of plan) {
+    if (queue.includes(x)) {
+      if (x !== queue.shift()) return "NO";
+    }
+  }
+  if (queue.length > 0) return "NO";
+  return answer;
+}
+
+let a = "CBA";
+let b = "CBDAGE";
+console.log(solution(a, b));
