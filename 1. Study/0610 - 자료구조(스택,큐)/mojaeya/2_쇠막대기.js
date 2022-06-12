c393887e156a289c12a820a799165aa15d2b7cd3
mojaeya <mojaeya@gmail.com>
2022-06-12 19:27:39 +0900

    fixed

--- 1. Study/0610 - 자료구조(스택,큐)/mojaeya/2_쇠막대기.js
+++ 1. Study/0610 - 자료구조(스택,큐)/mojaeya/2_쇠막대기.js
@@ -4,11 +4,12 @@ function solution(arr) {
   let answer = 0;
   let stack = [];
   for (let i = 0; i < arr.length; i++) {
-    if (x === "(") stack.push(arr[i]);
+    if (arr[i] === "(") stack.push(arr[i]);
     else {
       stack.pop();
       if (arr[i - 1] === "(") answer += stack.length; //레이저
       else answer += 1; // 막대기 끝
     }
   }
+  return answer;
 }
