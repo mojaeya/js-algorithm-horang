44671573b1a2bfe8dd26f5087cb81c7ce0ad9757
mojaeya <mojaeya@gmail.com>
2022-06-12 19:28:00 +0900

    0613 문제 정답

--- 2. Answer/0613 - 정렬, 그리디.js
+++ 2. Answer/0613 - 정렬, 그리디.js
@@ -1,13 +1,103 @@
-// 🐯 1.
+// 🐯 1. 선택 정렬
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) idx = j;
+    }
+    [arr[i], arr[idx]] = [arr[idx], arr[i]];
+  }
+  return answer;
+}
 
-// 🐯 2.
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
 
-// 🐯 3.
+// 🐯 2. 버블 정렬
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return answer;
+}
 
-// 🐯 4.
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
 
-// 🐯 5.
+// 🐯 3. Special Sort
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > 0 && arr[j + 1] < 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return answer;
+}
 
-// 🐯 6.
+let arr = [1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(arr));
 
-// 🐯 7.
+// 🐯 4. 삽입정렬
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = arr[i],
+      j;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > tmp) arr[j + 1] = arr[j];
+      else break;
+    }
+    arr[j + 1] = tmp;
+  }
+  return answer;
+}
+
+let arr = [11, 7, 5, 6, 10, 9];
+console.log(solution(arr));
+
+// 🐯 5. LRU
+function solution(size, arr) {
+  let answer = Array.from({ length: size }, () => 0);
+  arr.forEach((x) => {
+    let pos = -1;
+    for (let i = 0; i < size; i++) if (x === answer[i]) pos = i;
+    if (pos === -1) {
+      for (let i = size - 1; i >= 1; i--) {
+        answer[i] = answer[i - 1];
+      }
+    } else {
+      for (let i = pos; i >= 1; i--) {
+        answer[i] = answer[i - 1];
+      }
+    }
+    answer[0] = x;
+  });
+
+  return answer;
+}
+
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+console.log(solution(5, arr));
+
+// 🐯 6. 장난꾸러기 현수
+function solution(arr) {
+  let answer = [];
+  let sortArr = arr.slice();
+  sortArr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== sortArr[i]) answer.push(i + 1);
+  }
+  return answer;
+}
+
+let arr = [120, 125, 152, 130, 135, 135, 143, 127, 160];
+console.log(solution(arr));
