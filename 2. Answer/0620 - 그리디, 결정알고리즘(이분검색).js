49e3cb85c9ba0f9322bb48a4c4bc1eadb6d96a48
mojaeya <mojaeya@gmail.com>
2022-06-18 22:28:49 +0900

    0620 문제 정답

--- 2. Answer/0620 - 그리디, 결정알고리즘(이분검색).js
+++ 2. Answer/0620 - 그리디, 결정알고리즘(이분검색).js
@@ -1,13 +1,157 @@
-// 🐯 1.
+// 🐯 1. 좌표정렬
+function solution(arr) {
+  let answer = arr;
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    else return a[0] - b[0];
+  });
+  return answer;
+}
 
-// 🐯 2.
+let arr = [
+  [2, 7],
+  [1, 3],
+  [1, 2],
+  [2, 5],
+  [3, 6],
+];
+console.log(solution(arr));
 
-// 🐯 3.
+// 🐯 2. 회의실 배정
+function solution(meeting) {
+  let answer = 0;
+  meeting.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - b[1];
+  });
+  let et = 0;
+  for (let x of meeting) {
+    if (x[0] >= et) {
+      answer++;
+      et = x[1];
+    }
+  }
+  return answer;
+}
 
-// 🐯 4.
+let arr = [
+  [1, 4],
+  [2, 3],
+  [3, 5],
+  [4, 6],
+  [5, 7],
+];
+console.log(solution(arr));
 
-// 🐯 5.
+// 🐯 3. 결혼식
+function solution(times) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let T_line = [];
+  for (let x of times) {
+    T_line.push([x[0], "s"]);
+    T_line.push([x[1], "e"]);
+  }
+  T_line.sort((a, b) => {
+    if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+    else return a[0] - b[0];
+  });
+  let cnt = 0;
+  for (let x of T_line) {
+    if (x[1] === "s") cnt++;
+    else cnt--;
+    answer = Math.max(answer, cnt);
+  }
+  return answer;
+}
 
-// 🐯 6.
+let arr = [
+  [14, 18],
+  [12, 15],
+  [15, 20],
+  [20, 30],
+  [5, 14],
+];
+console.log(solution(arr));
 
-// 🐯 7.
+// 🐯 4. 이분검색
+function solution(target, arr) {
+  let answer;
+  arr.sort((a, b) => a - b);
+  let lt = 0,
+    rt = arr.length - 1;
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (arr[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (arr[mid] > target) rt = mid - 1;
+    else lt = mid + 1;
+  }
+
+  return answer;
+}
+
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+console.log(solution(32, arr));
+
+// 🐯 5. 뮤직비디오
+function count(songs, capacity) {
+  let cnt = 1,
+    sum = 0;
+  for (let x of songs) {
+    if (sum + x > capacity) {
+      cnt++;
+      sum = x;
+    } else sum += x;
+  }
+  return cnt;
+}
+
+function solution(m, songs) {
+  let answer;
+  let lt = Math.max(...songs);
+  let rt = songs.reduce((a, b) => a + b, 0);
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (count(songs, mid) <= m) {
+      answer = mid;
+      rt = mid - 1;
+    } else {
+      lt = mid + 1;
+    }
+  }
+  return answer;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(solution(3, arr));
+
+// 🐯 6. 마구간 정하기
+function count(stable, dist) {
+  let cnt = 1,
+    ep = stable[0];
+  for (let i = 1; i < stable.length; i++) {
+    if (stable[i] - ep >= dist) {
+      cnt++;
+      ep = stable[i];
+    }
+  }
+  return cnt;
+}
+function solution(c, stable) {
+  let answer;
+  stable.sort((a, b) => a - b);
+  let lt = 1;
+  let rt = stable[stable.length - 1];
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (count(stable, mid) >= c) {
+      answer = mid;
+      lt = mid + 1;
+    } else rt = mid - 1;
+  }
+  return answer;
+}
+
+let arr = [1, 2, 8, 4, 9];
+console.log(solution(3, arr));
