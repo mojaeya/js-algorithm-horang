6f7b574d07866d50c3f479b0dc2480d99b98b966
mojaeya <mojaeya@gmail.com>
2022-06-20 12:59:30 +0900

    0620 mojaeya

--- 1. Study/0620 - 그리디, 결정알고리즘(이분검색)/mojaeya/4_이분검색.js
+++ 1. Study/0620 - 그리디, 결정알고리즘(이분검색)/mojaeya/4_이분검색.js
@@ -1,20 +1,23 @@
 // 이진탐색은 오름차순 혹은 내림차순으로 정렬되어있어야 찾을 수 있는 전제조건이 있다.
+// 순차탐색처럼 처음부터 끝까지 하나씩 모든 데이터를 체크하여 값을 찾는 것이 아니라
+//  탐색 범위를 절반씩 줄여가며 찾아가는 Search 방법이다.
 
 let arr = [23, 87, 65, 12, 57, 32, 99, 81];
 console.log(solution(8, 32, arr));
 
 function solution(N, M, arr) {
   arr.sort((a, b) => a - b);
-  let low = 0;
-  let high = N - 1;
+  // console.log(arr);
+  let lt = 0;
+  let rt = N - 1;
 
-  while (low <= high) {
-    let midIdx = Math.floor((low + high) / 2);
-    //console.log("midIdx :",midIdx)
+  while (lt <= rt) {
+    let midIdx = Math.floor((lt + rt) / 2);
+    // console.log("midIdx :", midIdx);
     let mid = arr[midIdx];
-    //console.log("mid :", mid)
+    // console.log("mid :", mid);
     if (mid === M) return arr.indexOf(mid) + 1;
-    else if (mid > M) high = midIdx - 1;
-    else low = midIdx + 1;
+    else if (mid > M) rt = midIdx - 1;
+    else lt = midIdx + 1;
   }
 }
--- 1. Study/0620 - 그리디, 결정알고리즘(이분검색)/mojaeya/6_마구간.js
+++ 1. Study/0620 - 그리디, 결정알고리즘(이분검색)/mojaeya/6_마구간.js
@@ -14,17 +14,26 @@ function solution(N, C, arr) {
     let mid = Math.floor((lt + rt) / 2); // 가장 가까운 두 말의 최대 거리라고 가정
     let horse = 1;
     let endPoint = arr[0];
-    //console.log(`lt = ${lt}, mid = ${mid}, rt = ${rt}`)
+    // console.log(`lt = ${lt}, mid = ${mid}, rt = ${rt}`);
 
     for (let i = 1; i < N; i++) {
       if (arr[i] - endPoint >= mid) {
-        //console.log("배치할 말 좌표(",arr[i],") -","배치된 말 좌표(",endPoint,") >=",mid, "")
+        // console.log(
+        //   "배치할 말 좌표(",
+        //   arr[i],
+        //   ") -",
+        //   "배치된 말 좌표(",
+        //   endPoint,
+        //   ") >=",
+        //   mid,
+        //   ""
+        // );
         horse++;
         endPoint = arr[i];
       }
     }
-    //console.log("배치 가능한 말 :",horse)
-    //console.log("================================")
+    // console.log("배치 가능한 말 :", horse);
+    // console.log("================================");
 
     if (horse >= C) {
       result = mid; // 조건에 부합하지만 더 최적의 값을 찾기 위해
