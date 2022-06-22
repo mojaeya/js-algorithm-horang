43f84bd1ad658d0a093e931fcd65cc5146873fe7
mojaeya <mojaeya@gmail.com>
2022-06-23 03:58:53 +0900

    0624 문제 정답

--- 2. Answer/0624 - 재귀함수와 완전탐색(DFS).js
+++ 2. Answer/0624 - 재귀함수와 완전탐색(DFS).js
@@ -1,13 +1,70 @@
-// 🐯 1.
+// 🐯 1. 재귀함수
+function solution(n) {
+  function DFS(L) {
+    if (L == 0) return;
+    else {
+      DFS(L - 1);
+      console.log(L);
+    }
+  }
+  DFS(n);
+}
 
-// 🐯 2.
+solution(3);
 
-// 🐯 3.
+// 🐯 2. 이진수 출력
+function solution(n) {
+  let answer = "";
+  function DFS(n) {
+    if (n === 0) return;
+    else {
+      DFS(parseInt(n / 2));
+      answer += String(n % 2);
+    }
+  }
+  DFS(n);
+  return answer;
+}
 
-// 🐯 4.
+console.log(solution(11));
 
-// 🐯 5.
+// 🐯 3. 이진트리순회(깊이우선탐색)
+function solution(n) {
+  let answer = "";
+  function DFS(v) {
+    if (v > 7) return;
+    else {
+      answer += v + " ";
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+    }
+  }
+  DFS(n);
+  return answer;
+}
 
-// 🐯 6.
+console.log(solution(1));
 
-// 🐯 7.
+// 🐯 4. 부분집합 구하기(DFS)
+function solution(n) {
+  let answer = [];
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  function DFS(L) {
+    if (L === n + 1) {
+      let tmp = "";
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 1) tmp += i + " ";
+      }
+      if (tmp.length > 0) answer.push(tmp.trim());
+    } else {
+      ch[L] = 1;
+      DFS(L + 1);
+      ch[L] = 0;
+      DFS(L + 1);
+    }
+  }
+  DFS(1);
+  return answer;
+}
+
+console.log(solution(3));
