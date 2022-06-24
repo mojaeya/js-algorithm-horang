f9d5cee094cb2ce3393d08db429ece2dca69e2b2
mojaeya <mojaeya@gmail.com>
2022-06-24 11:22:14 +0900

    0627 문제 정답

--- 2. Answer/0627 - 재귀함수와 완전탐색(DFS).js
+++ 2. Answer/0627 - 재귀함수와 완전탐색(DFS).js
@@ -1,13 +1,136 @@
-// 🐯 1.
+// 🐯 1. 합이 같은 부분집합
+function solution(arr) {
+  let answer = "NO",
+    flag = 0;
+  let total = arr.reduce((a, b) => a + b, 0);
+  let n = arr.length;
+  function DFS(L, sum) {
+    if (flag) return;
+    if (L === n) {
+      if (total - sum === sum) {
+        answer = "YES";
+        flag = 1;
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
 
-// 🐯 2.
+let arr = [1, 3, 5, 6, 7, 10];
+console.log(solution(arr));
 
-// 🐯 3.
+// 🐯 2. 바둑이 승차
+function solution(c, arr) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = arr.length;
+  function DFS(L, sum) {
+    if (sum > c) return;
+    if (L === n) {
+      answer = Math.max(answer, sum);
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
 
-// 🐯 4.
+let arr = [81, 58, 42, 33, 61];
+console.log(solution(259, arr));
 
-// 🐯 5.
+// 🐯 3. 최대점수 구하기
+function solution(m, ps, pt) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = ps.length;
+  function DFS(L, sum, time) {
+    if (time > m) return;
+    if (L === n) {
+      answer = Math.max(answer, sum);
+    } else {
+      DFS(L + 1, sum + ps[L], time + pt[L]);
+      DFS(L + 1, sum, time);
+    }
+  }
 
-// 🐯 6.
+  DFS(0, 0, 0);
+  return answer;
+}
 
-// 🐯 7.
+let ps = [10, 25, 15, 6, 7];
+let pt = [5, 12, 8, 3, 4];
+console.log(solution(20, ps, pt));
+
+// 🐯 4. 중복순열
+function solution(n, m) {
+  let answer = [];
+  let tmp = Array.from({ length: m }, () => 0);
+  function DFS(L) {
+    if (L === m) {
+      answer.push(tmp.slice());
+    } else {
+      for (let i = 1; i <= n; i++) {
+        tmp[L] = i;
+        DFS(L + 1);
+      }
+    }
+  }
+
+  DFS(0);
+  return answer;
+}
+
+console.log(solution(3, 2));
+
+// 🐯 5. 동전교환
+function solution(m, arr) {
+  let answer = Number.MAX_SAFE_INTEGER;
+  let n = arr.length;
+  function DFS(L, sum) {
+    if (sum > m) return;
+    if (L >= answer) return;
+    if (sum === m) {
+      answer = Math.min(answer, L);
+    } else {
+      for (let i = 0; i < n; i++) {
+        DFS(L + 1, sum + arr[i]);
+      }
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
+
+let arr = [1, 2, 5];
+console.log(solution(15, arr));
+
+// 🐯 6. 순열 구하기
+function solution(m, arr) {
+  let answer = [];
+  n = arr.length;
+  let ch = Array.from({ length: n }, () => 0);
+  let tmp = Array.from({ length: m }, () => 0);
+  function DFS(L) {
+    if (L === m) {
+      answer.push(tmp.slice());
+    } else {
+      for (let i = 0; i < n; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          tmp[L] = arr[i];
+          DFS(L + 1);
+          ch[i] = 0;
+        }
+      }
+    }
+  }
+  DFS(0);
+  return answer;
+}
+
+let arr = [3, 6, 9];
+console.log(solution(2, arr));
