e79826bc0caef814f3102a1b378d15b020109c12
mojaeya <mojaeya@gmail.com>
2022-06-27 12:22:09 +0900

    0701 문제 정답

--- 2. Answer/0701 - 재귀함수와 완전탐색(DFS).js
+++ 2. Answer/0701 - 재귀함수와 완전탐색(DFS).js
@@ -1,13 +1,106 @@
-// 🐯 1.
+// 🐯 1. 팩토리얼
+function solution(n) {
+  let answer;
+  function DFS(n) {
+    if (n === 1) return 1;
+    else return n * DFS(n - 1);
+  }
+  answer = DFS(n);
+  return answer;
+}
 
-// 🐯 2.
+console.log(solution(5));
 
-// 🐯 3.
+// 🐯 2. 조합수(메모이제이션)
+function solution(n, r) {
+  let answer = [];
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
 
-// 🐯 4.
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+  }
+  answer = DFS(n, r);
+  return answer;
+}
 
-// 🐯 5.
+console.log(solution(5, 3));
 
-// 🐯 6.
+// 🐯 3. 수열 추측하기
+function solution(n, f) {
+  let answer,
+    flag = 0;
+  let dy = Array.from(Array(11), () => Array(11).fill(0));
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  let p = Array.from({ length: n }, () => 0);
+  let b = Array.from({ length: n }, () => 0);
+  function combi(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
+  }
+  function DFS(L, sum) {
+    if (flag) return;
+    if (L === n && sum === f) {
+      answer = p.slice();
+      flag = 1;
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          p[L] = i;
+          DFS(L + 1, sum + b[L] * p[L]);
+          ch[i] = 0;
+        }
+      }
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    b[i] = combi(n - 1, i);
+  }
+  DFS(0, 0);
+  return answer;
+}
 
-// 🐯 7.
+console.log(solution(4, 16));
+
+// 🐯 4. 조합 구하기
+function solution(n, m) {
+  let answer = [];
+  let tmp = Array.from({ length: m }, () => 0);
+  function DFS(L, s) {
+    if (L === m) {
+      answer.push(tmp.slice());
+    } else {
+      for (let i = s; i <= n; i++) {
+        tmp[L] = i;
+        DFS(L + 1, i + 1);
+      }
+    }
+  }
+  DFS(0, 1);
+  return answer;
+}
+
+console.log(solution(4, 2));
+
+// 🐯 5. 수들의 조합
+function solution(n, k, arr, m) {
+  let answer = 0;
+  function DFS(L, s, sum) {
+    if (L === k) {
+      if (sum % m === 0) answer++;
+    } else {
+      for (let i = s; i < n; i++) {
+        DFS(L + 1, i + 1, sum + arr[i]);
+      }
+    }
+  }
+
+  DFS(0, 0, 0);
+  return answer;
+}
+
+let arr = [2, 4, 5, 8, 12];
+console.log(solution(5, 3, arr, 6));
