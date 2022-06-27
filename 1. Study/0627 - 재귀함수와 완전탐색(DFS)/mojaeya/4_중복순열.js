47283a2b611d46ab23cd996f49f9f5aeb8034857
mojaeya <mojaeya@gmail.com>
2022-06-27 12:22:23 +0900

    0627 mojaeya

--- 1. Study/0627 - 재귀함수와 완전탐색(DFS)/mojaeya/4_중복순열.js
+++ 1. Study/0627 - 재귀함수와 완전탐색(DFS)/mojaeya/4_중복순열.js
@@ -24,25 +24,25 @@ function solution(N, M) {
   return [answer, cnt];
 }
 
-function solution(N, M) {
-  let tmp = Array.from({ length: M }, () => 0);
-  let answer = [];
-  let cnt = 0;
-  function DFS(L) {
-    if (L === M) {
-      console.log("DFS(", L, ") 탐색 종료");
-      answer.push([...tmp]); // tmp.slice()
-      cnt++;
-    } else {
-      for (let i = 1; i <= N; i++) {
-        console.log("DFS(", L, ") for loop :", i);
-        console.log("tmp[", L, "]", "=", i);
-        tmp[L] = i;
-        DFS(L + 1);
-        console.log("DFS(", L, ") 이전 주소 복귀, for loop :", i + 1);
-      }
-    }
-  }
-  DFS(0);
-  return [answer, cnt];
-}
+// function solution(N, M) {
+//   let tmp = Array.from({ length: M }, () => 0);
+//   let answer = [];
+//   let cnt = 0;
+//   function DFS(L) {
+//     if (L === M) {
+//       console.log("DFS(", L, ") 탐색 종료");
+//       answer.push([...tmp]); // tmp.slice()
+//       cnt++;
+//     } else {
+//       for (let i = 1; i <= N; i++) {
+//         console.log("DFS(", L, ") for loop :", i);
+//         console.log("tmp[", L, "]", "=", i);
+//         tmp[L] = i;
+//         DFS(L + 1);
+//         console.log("DFS(", L, ") 이전 주소 복귀, for loop :", i + 1);
+//       }
+//     }
+//   }
+//   DFS(0);
+//   return [answer, cnt];
+// }
--- 1. Study/0627 - 재귀함수와 완전탐색(DFS)/mojaeya/6_순열구하기.js
+++ 1. Study/0627 - 재귀함수와 완전탐색(DFS)/mojaeya/6_순열구하기.js
@@ -6,29 +6,6 @@ let arr = [3, 6, 9]; // 주어진 자연수
 
 console.log(solution(N, M, arr));
 
-// function solution(N, M, arr) {
-//     let answer = []
-//     let check = Array.from({ length: N }, () => 0) // [0,0,0]
-//     let tmp = Array.from({ length: M }, () => 0) // [0,0]
-
-//     function DFS(L) {
-//       if (L === M) {
-//         answer.push(tmp.slice());
-//       } else {
-//         for (let i = 0; i < N; i++) {
-//           if (check[i] === 0) { // 이게 1이면 그 해당 인덱스의 arr 숫자가 쓰인것.
-//             check[i] = 1;  // 노드 방문 체크
-//             tmp[L] = arr[i];
-//             DFS(L + 1);
-//             check[i] = 0; // 노드 방문 후 돌아갈 때 리셋
-//           }
-//         }
-//       }
-//     }
-//     DFS(0);
-//     return answer;
-// }
-
 function solution(N, M, arr) {
   let answer = [];
   let check = Array.from({ length: N }, () => 0); // [0,0,0]
@@ -36,22 +13,15 @@ function solution(N, M, arr) {
 
   function DFS(L) {
     if (L === M) {
-      console.log("DFS(", L, ") 탐색 종료");
       answer.push(tmp.slice());
     } else {
       for (let i = 0; i < N; i++) {
-        console.log("DFS(", L, ")");
-        console.log("for loop :", i);
         if (check[i] === 0) {
-          console.log("before check :", check);
-          check[i] = 1;
-          console.log("after check :", check);
+          // 이게 1이면 그 해당 인덱스의 arr 숫자가 쓰인것.
+          check[i] = 1; // 노드 방문 체크
           tmp[L] = arr[i];
-          console.log("tmp :", tmp);
           DFS(L + 1);
-          console.log("DFS(", L, ") 이전 주소 복귀, for loop :", i);
-          check[i] = 0;
-          console.log("reset check :", check);
+          check[i] = 0; // 노드 방문 후 돌아갈 때 리셋
         }
       }
     }
@@ -59,3 +29,34 @@ function solution(N, M, arr) {
   DFS(0);
   return answer;
 }
+
+// function solution(N, M, arr) {
+//   let answer = [];
+//   let check = Array.from({ length: N }, () => 0); // [0,0,0]
+//   let tmp = Array.from({ length: M }, () => 0); // [0,0]
+
+//   function DFS(L) {
+//     if (L === M) {
+//       console.log("DFS(", L, ") 탐색 종료");
+//       answer.push(tmp.slice());
+//     } else {
+//       for (let i = 0; i < N; i++) {
+//         console.log("DFS(", L, ")");
+//         console.log("for loop :", i);
+//         if (check[i] === 0) {
+//           console.log("before check :", check);
+//           check[i] = 1;
+//           console.log("after check :", check);
+//           tmp[L] = arr[i];
+//           console.log("tmp :", tmp);
+//           DFS(L + 1);
+//           console.log("DFS(", L, ") 이전 주소 복귀, for loop :", i);
+//           check[i] = 0;
+//           console.log("reset check :", check);
+//         }
+//       }
+//     }
+//   }
+//   DFS(0);
+//   return answer;
+// }
