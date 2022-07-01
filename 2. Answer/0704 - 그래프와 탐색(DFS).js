2f88ea27be81fae333e7684bbe34bd38535a5cd4
mojaeya <mojaeya@gmail.com>
2022-07-01 15:06:22 +0900

    0704 문제 정답

--- 2. Answer/0704 - 그래프와 탐색(DFS).js
+++ 2. Answer/0704 - 그래프와 탐색(DFS).js
@@ -1,13 +1,124 @@
-// 🐯 1.
+// 🐯 1. 경로탐색(인접행렬)
+function solution(n, arr) {
+  let answer = 0;
+  let graph = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  path = [];
+  for (let [a, b] of arr) {
+    graph[a][b] = 1;
+  }
+  function DFS(v) {
+    if (v === n) {
+      answer++;
+      console.log(path);
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (graph[v][i] === 1 && ch[i] === 0) {
+          ch[i] = 1;
+          path.push(i);
+          DFS(i);
+          ch[i] = 0;
+          path.pop();
+        }
+      }
+    }
+  }
 
-// 🐯 2.
+  path.push(1);
+  ch[1] = 1;
+  DFS(1);
+  return answer;
+}
 
-// 🐯 3.
+let arr = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+console.log(solution(5, arr));
 
-// 🐯 4.
+// 🐯 2. 경로탐색(인접리스트)
+function solution(n, arr) {
+  let answer = 0;
+  let graph = Array.from(Array(n + 1), () => Array());
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  let path = [];
+  for (let [a, b] of arr) {
+    graph[a].push(b);
+  }
+  function DFS(v) {
+    if (v === n) {
+      answer++;
+      console.log(path);
+    } else {
+      for (let nv of graph[v]) {
+        if (ch[nv] === 0) {
+          path.push(nv);
+          ch[nv] = 1;
+          DFS(nv);
+          ch[nv] = 0;
+          path.pop();
+        }
+      }
+    }
+  }
+  ch[1] = 1;
+  path.push(1);
+  DFS(1);
+  return answer;
+}
 
-// 🐯 5.
+let arr = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+console.log(solution(5, arr));
 
-// 🐯 6.
+// 🐯 3. 미로탐색
+function solution(board) {
+  let answer = 0;
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+  function DFS(x, y) {
+    if (x === 6 && y === 6) answer++;
+    else {
+      for (let k = 0; k < 4; k++) {
+        let nx = x + dx[k];
+        let ny = y + dy[k];
+        if (nx >= 0 && nx <= 6 && ny >= 0 && ny <= 6 && board[nx][ny] === 0) {
+          board[nx][ny] = 1;
+          DFS(nx, ny);
+          board[nx][ny] = 0;
+        }
+      }
+    }
+  }
+  board[0][0] = 1;
+  DFS(0, 0);
+  return answer;
+}
 
-// 🐯 7.
+let arr = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 0, 1, 0, 1, 1],
+  [1, 1, 0, 0, 0, 0, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0],
+];
+
+console.log(solution(arr));
