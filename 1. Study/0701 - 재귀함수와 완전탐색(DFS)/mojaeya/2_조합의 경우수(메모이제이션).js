c91c5f0dc26b273cb2c5af61e465b0ab65b6fdea
mojaeya <mojaeya@gmail.com>
2022-07-02 22:49:59 +0900

    0701 mojaeya

--- 1. Study/0701 - 재귀함수와 완전탐색(DFS)/mojaeya/2_조합의 경우수(메모이제이션).js
+++ 1. Study/0701 - 재귀함수와 완전탐색(DFS)/mojaeya/2_조합의 경우수(메모이제이션).js
@@ -14,17 +14,17 @@ function solution(n, r) {
 // 메모이제이션
 // 동일한 계산을 반복해야 할 때, 이전에 계산한 값을 메모리에 저장함으로써 동일한 계산의 반복 수행을 제거하여 프로그램 실행 속도를 빠르게 하는 기술이다. 동적 계획법의 핵심이 되는 기술
 
-// function solution(n, r) {
-//     let answer = [];
-//     let dy = Array.from(Array(35), () => Array(35).fill(0));
+function solution(n, r) {
+  let answer = [];
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
 
-//     function DFS(n, r) {
-//       if (dy[n][r] > 0) return dy[n][r];
-//       if (n === r || r === 0) return 1;
-//       else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
-//     }
-//     answer = DFS(n, r);
-//     return answer;
-//   }
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+  }
+  answer = DFS(n, r);
+  return answer;
+}
 
-//   console.log(solution(5, 3));
+console.log(solution(5, 3));
