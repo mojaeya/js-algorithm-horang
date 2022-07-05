553f99e1c2c618e6086f4243f9b2bf2f1e9d09e0
mojaeya <mojaeya@gmail.com>
2022-07-05 11:38:29 +0900

    0708 문제 정답

--- 2. Answer/0708 - 그래프와 탐색(BFS).js
+++ 2. Answer/0708 - 그래프와 탐색(BFS).js
@@ -1,13 +1,127 @@
-// 🐯 1.
+// 🐯 1. 이진트리 넓이우선탐색(BFS)
+function solution() {
+  let answer = "";
+  let queue = [];
+  queue.push(1);
+  while (queue.length) {
+    console.log(queue);
+    let v = queue.shift();
+    answer += v + " ";
+    for (let nv of [v * 2, v * 2 + 1]) {
+      if (nv > 7) continue;
+      queue.push(nv);
+    }
+  }
+  return answer;
+}
 
-// 🐯 2.
+console.log(solution());
 
-// 🐯 3.
+// 🐯 2. 송아지 찾기(BFS : 상태트리탐색)
+function solution(s, e) {
+  let answer = 0;
+  let ch = Array.from({ length: 10001 }, () => 0);
+  let dis = Array.from({ length: 10001 }, () => 0);
+  let queue = [];
+  queue.push(s);
+  ch[s] = 1;
+  dis[s] = 0;
+  while (queue.length) {
+    let x = queue.shift();
+    for (let nx of [x - 1, x + 1, x + 5]) {
+      if (nx === e) return dis[x] + 1;
+      if (nx > 0 && nx <= 10000 && ch[nx] === 0) {
+        ch[nx] = 1;
+        queue.push(nx);
+        dis[nx] = dis[x] + 1;
+      }
+    }
+  }
+  return answer;
+}
 
-// 🐯 4.
+console.log(solution(8, 3));
 
-// 🐯 5.
+// 🐯 3. 섬나라 아일랜드(DFS, BFS)
 
-// 🐯 6.
+// DFS
+function solution(board) {
+  let answer = 0;
+  let n = board.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+  function DFS(x, y) {
+    board[x][y] = 0;
+    for (let k = 0; k < 8; k++) {
+      let nx = x + dx[k];
+      let ny = y + dy[k];
+      if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+        DFS(nx, ny);
+      }
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        answer++;
+        DFS(i, j);
+      }
+    }
+  }
+  return answer;
+}
 
-// 🐯 7.
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+
+console.log(solution(arr));
+
+// BFS
+function solution(board) {
+  let answer = 0;
+  let n = board.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+  let queue = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        queue.push([i, j]);
+        answer++;
+        while (queue.length) {
+          let x = queue.shift();
+          for (let k = 0; k < 8; k++) {
+            let nx = x[0] + dx[k];
+            let ny = x[1] + dy[k];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+              board[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
+      }
+    }
+  }
+  return answer;
+}
+
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+
+console.log(solution(arr));
